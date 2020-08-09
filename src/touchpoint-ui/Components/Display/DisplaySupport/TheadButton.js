8ad040d7f53f11c6b986faf96fc79a829fded32a
Youssof Fahmy <youssoffahmy@gmail.com>
2020-08-08 21:28:53 -0400

    checkpoint

--- src/touchpoint-ui/Components/Display/DisplaySupport/TheadButton.js
+++ src/touchpoint-ui/Components/Display/DisplaySupport/TheadButton.js
@@ -35,7 +35,7 @@ export default function TheadButton(props){
 					noFilter = {props.noFilter}
 					openTrigger = {openTrigger}
 				/>}
-				menuStyle = {{maxHeight: '170px'}}
+				menuStyle = {{maxHeight: '20vh'}}
 			>
 				{props.children}
 				<span className = {iconClass}>{icon}</span>			
