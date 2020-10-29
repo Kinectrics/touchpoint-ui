c31f9c258a525adfcdeb93e8cc9071302469fe9a
Youssof Fahmy <youssoffahmy@gmail.com>
2020-10-29 19:20:02 -0400

    styling for dropdown cells that have badges

--- src/Modules/VendorRubrics/VendorRubrics.js
+++ src/Modules/VendorRubrics/VendorRubrics.js
@@ -40,7 +40,7 @@ export default function VendorRubrics(){
 		{headerID: 'id',displayName:'ID', width: 100, required: true},
 		{headerID: 'project', displayName: 'Project', width: 100, options:[2,100,200], onEdit:(e)=>false},
 		{headerID: 'projectName', displayName:'Project Name', width: 220},
-		{headerID: 'status', displayName: 'Status', width: 200, required: true, styling: statusStyle, onEdit: ()=>true},
+		{headerID: 'status', displayName: 'Status', width: 200, required: true, styling: statusStyle, onEdit: ()=>true, options: ['Complete', 'Pending Approval', 'In Progress']},
 		{headerID: 'due', displayName: 'Due', width: 150, type: 'date', onEdit: ({cellValue})=>console.log({cellValue})},
 		{headerID: 'SM', displayName:'SM', width: 200, onEdit: editHandler},
 		{headerID: 'intern', displayName:'Intern', width: 300, onEdit: ()=>true},
