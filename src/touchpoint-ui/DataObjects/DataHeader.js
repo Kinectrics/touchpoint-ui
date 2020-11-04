f11059b0576e4f3500d194ea8b44af26ba1b2815
Youssof Fahmy <youssoffahmy@gmail.com>
2020-11-04 12:16:13 -0500

    checkpoint

--- src/touchpoint-ui/DataObjects/DataHeader.js
+++ src/touchpoint-ui/DataObjects/DataHeader.js
@@ -1,9 +1,9 @@
 import DataFilter from './DataFilter'
 import DataType from './DataType'
 
-export default class DataHeader{
-	
-	constructor(options){
+export default class DataHeader {
+
+	constructor(options) {
 		this.headerID = options.headerID
 		this.displayName = options.displayName
 		this.width = options.width ? options.width : 100
@@ -16,73 +16,74 @@ export default class DataHeader{
 		this.uniqueValues = {}
 		this.type = options.type ? options.type : 'string'
 		this.onClick = options.onClick
-		this.options=options.options
-		this.position = options.position ? options.position : 0
-		
-		this.component = options.component 
+		this.options = options.options
+		this.position = options.index
+		this.after = options.after
+
+		this.component = options.component
 		this.props = options.props
-		
+
 		//Default filter list only has 1 functions (array filter)
 		this.filterList = {
 			arrayFilter: {
 				func: (cellValue) => {
 					return (this.uniqueValues[cellValue] || this.uniqueValues[cellValue] === undefined)
-				}, 
+				},
 			},
 		}
-		
+
 		this.dataType = new DataType(this.type)
 		this.format = this.dataType.format
 		this.parse = this.dataType.parse
 		this.compare = this.dataType.compare
 	}
 
-	
-	filter(cellValue, dataRow){
+
+	filter(cellValue, dataRow) {
 		//Check if any of the filters fail
 		//If any filters fail, res will be true. Return the opposite
-		return !Object.keys(this.filterList).some( (f)=>{
+		return !Object.keys(this.filterList).some((f) => {
 			return !this.filterList[f].func(cellValue, dataRow)
 		})
 	}
-	
-	
-	addFilter(options){
-		this.filterList[options.id] = new DataFilter({...options, headerType: this.type})
+
+
+	addFilter(options) {
+		this.filterList[options.id] = new DataFilter({ ...options, headerType: this.type })
 	}
-	
-	
-	removeFilter(filterID){
+
+
+	removeFilter(filterID) {
 		delete this.filterList[filterID]
 	}
-	
-	
+
+
 	//Saves a list of unique values in the column - to be used in the filter dropdowns
-	embedData(data, metaData){
+	embedData(data, metaData) {
 		this.uniqueValues = uniqueByColumn(data, metaData, this.headerID, this.uniqueValues)
 	}
-	
-	selectAll(setVal){
-		Object.keys(this.uniqueValues).map((uv)=>{
+
+	selectAll(setVal) {
+		Object.keys(this.uniqueValues).map((uv) => {
 			this.uniqueValues[uv] = setVal
 		})
 	}
-	
+
 	//checks if there are any active filters, including the array filter
-	hasFilter(){
+	hasFilter() {
 		//array filter is always there, check if any other filters are there
 		let testRes = Object.keys(this.filterList).length === 1
-		
+
 		//if there are no other filters, check if the array filter is active
-		if(testRes){
+		if (testRes) {
 			Object.keys(this.uniqueValues).forEach((val) => {
-				if (!this.uniqueValues[val]){testRes = false}
+				if (!this.uniqueValues[val]) { testRes = false }
 			})
 		}
 		return !testRes
 	}
-	
-	clearFilter(){
+
+	clearFilter() {
 		this.filterList = {
 			arrayFilter: {
 				func: (cellValue) => {
@@ -98,24 +99,24 @@ export default class DataHeader{
 //Returns an array of unique values from a column of DataRow objects
 function uniqueByColumn(data, metaData, columnID, oldValues) {
 	const res = {}
-	
+
 	data.forEach((r, idx) => {
-		
-		if (metaData[idx] && (metaData[idx].visible || metaData[idx].filteredBy === columnID + ';')){
+
+		if (metaData[idx] && (metaData[idx].visible || metaData[idx].filteredBy === columnID + ';')) {
 			//New vales added as true, old values keep their value
-			if (oldValues[r[columnID]] === undefined){
+			if (oldValues[r[columnID]] === undefined) {
 				res[r[columnID]] = true
-			} else{
+			} else {
 				res[r[columnID]] = oldValues[r[columnID]]
 			}
 		}
 	})
-	
+
 	const orderedRes = {}
-	
-	Object.keys(res).sort().forEach((key)=>{
+
+	Object.keys(res).sort().forEach((key) => {
 		orderedRes[key] = res[key]
 	})
-	
+
 	return orderedRes
 }
