75a1514f785c588a59daf4e9c0210e087ece3efa
Youssof Fahmy <youssoffahmy@gmail.com>
2020-11-10 18:26:51 -0500

    release

--- src/touchpoint-ui/Components/Display/DisplaySupport/TableBody.js
+++ src/touchpoint-ui/Components/Display/DisplaySupport/TableBody.js
@@ -28,7 +28,6 @@ export default function TableBody(props) {
 
 					let renderRow = dr !== []
 					
-					
 					if (props.searchable && props.metaData[idx]) {
 						renderRow = !props.metaData[idx].searchHidden
 					}
