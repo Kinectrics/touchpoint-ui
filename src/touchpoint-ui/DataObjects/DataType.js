0f1f2ab6d854e239c058c07b967f31374e046fb0
gitbot92 <57538880+gitbot92@users.noreply.github.com>
2020-11-18 15:21:16 -0500

    dates fixed

--- src/touchpoint-ui/DataObjects/DataType.js
+++ src/touchpoint-ui/DataObjects/DataType.js
@@ -11,7 +11,10 @@ const formatFunctions = {
 //Takes a user input and changes it to the correct data type (eg. Date string to new Date object)
 const parseFunctions = {
 	date: (input) => {
+		
 		const testVal = input.toString().toLowerCase()
+
+		if(testVal.trim() === '') { return ''}
 		
 		if (testVal === 'today' || testVal === 'td') { return moment().toISOString() }
 		
